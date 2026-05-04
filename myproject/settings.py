72896341e86548d45717a0cef037928253056237
abusufiyan7518 <abusufiyantechsak@example.com>
2026-05-05 01:30:18 +0530

    Add DatabAse

--- myproject/settings.py
+++ myproject/settings.py
@@ -87,13 +87,19 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 # ========================
 
-DATABASES = {
-'default': {
-'ENGINE': 'django.db.backends.sqlite3',
-'NAME': BASE_DIR / 'db.sqlite3',
-}
-}
+# DATABASES = {
+# 'default': {
+# 'ENGINE': 'django.db.backends.sqlite3',
+# 'NAME': BASE_DIR / 'db.sqlite3',
+# }
+# }
 
+import dj_database_url
+import os
+
+DATABASES = {
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+}
 # ========================
 
 # PASSWORD VALIDATION
